3e61177b21e7a5f2c5a5af19ab019309b5e8e381
LevMewMew <iloveriba1@gmail.com>
2026-07-14 16:39:47 +0500

    a

--- node_2d.gd
+++ node_2d.gd
@@ -1,6 +1,11 @@
 extends Node2D
+func pepe() -> int:
+	var e1 = 1
+	var e2 = 2
+	var e3 = e1 + e2
+	return e3
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	print("sdifhjids")
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("ui_down"):
+		print(pepe())
+	print(500 * delta)
